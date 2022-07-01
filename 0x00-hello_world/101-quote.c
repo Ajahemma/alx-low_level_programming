0e3d39e13bae25afb89e3cd2be97abd228a1a768
Ajah Emmanuel <ajahemma@protonmail.com>
2022-07-01 06:09:11 -0700

    My Ninth Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 
-	write (2, qoute, 59);
+	write (2, qout, 59);
 	return(1);
 }
