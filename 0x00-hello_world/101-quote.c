da01bb28cade137dbff1ac8b61dd9b444483c24e
Ajah Emmanuel <ajahemma@protonmail.com>
2022-06-30 17:24:52 -0700

    My Ninth Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,6 +8,9 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
+	write (2, qoute, 59);
 	return(1);
 }
