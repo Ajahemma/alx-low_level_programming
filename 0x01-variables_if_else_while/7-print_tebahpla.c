d125b6c6a5986f22f409f04984210e9119269181
Ajah Emmanuel <ajahemma@protonmail.com>
2022-07-05 07:41:38 -0700

    My eight commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: Alphabets
+ * Return : zero on success
+ */
+
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar (letter);
+		letter--;
+	}
+	putchar('\n');
+
+	return(0);
+}
