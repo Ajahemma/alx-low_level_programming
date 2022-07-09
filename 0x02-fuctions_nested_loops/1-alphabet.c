78c2a494407a2e4470ce91efdb02c9da33768f4c
Ajah Emmanuel <ajahemma@protonmail.com>
2022-07-09 05:57:12 -0700

    My second commit

---
+++ 0x02-fuctions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet _ prints alphabet in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+}
